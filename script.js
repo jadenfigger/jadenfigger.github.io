a81ad326210a7dbcc28453227f6ef8c1c67fd101
Jaden Figger <jadenfigger@gmail.com>
2022-01-19 13:56:12 -0700

    commit on main

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ let ctx = canvas.getContext("2d");
 window.addEventListener("keydown", keyPressed, false);
 
 let sButton = document.querySelector("#sButton");
+let scoreLabel = document.querySelector("#score");
 
 let start, pTimeStep;
 let gameRunning = false;
+let gameScore = 0;
 
 let gridState = [[0, 0, 0, 0],
 				 [0, 0, 2, 0],
@@ -14,8 +16,7 @@ let gridState = [[0, 0, 0, 0],
 				 [0, 0, 0, 0]];
 
 
-
-				 let gridColorStates = {0: "#cdc0b4", 2: "#eee4da", 4: "#ede0c8", 8: "#f2b179", 16: "#f59563", 32: "#f67c5f"};
+let gridColorStates = {0: "#cdc0b4", 2: "#eee4da", 4: "#ede0c8", 8: "#f2b179", 16: "#f59563", 32: "#f67c5f", 64: "#f65e3b", 128: "#edcf72", 256: "#edcf72", 512: "#edcf72", 1024: "#edc53f", 2048: "#edc22e"};
 let cellSpacing = 20;
 let cellWidth = 700/4;
 
@@ -23,8 +24,6 @@ function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 	drawGrid();	
-
-
 }
 
 function drawGrid() {
@@ -46,14 +45,29 @@ function drawGrid() {
 	}
 }
 
+function isBoardFull() {
+	let con = true;
+	gridState.forEach((outerArray) => {outerArray.forEach((element) => {if (element==0) con=false})});
+	
+	return con;
+}
+
 function addTwoOrFour() {
+	if (isBoardFull()) {
+		return;
+	}
+	
+	gridState.forEach((outerArray) => {outerArray.forEach((element) => {if (element==0) con=true})});
+	if (!con) {
+		return;
+	}
+
 	let x = Math.floor(Math.random() * 4);
 	let y = Math.floor(Math.random() * 4);
 
-	console.log(gridState);
 	while (gridState[y][x] != 0) {
-		x = Math.random() * 4;
-		y = Math.random() * 4;
+		x = Math.floor(Math.random() * 4);
+		y = Math.floor(Math.random() * 4);
 	}
 	if (Math.random() < 0.5) {
 		gridState[y][x] = 2;
@@ -62,9 +76,13 @@ function addTwoOrFour() {
 	}
 }
 
+function arraysEqual(a1,a2) {
+    /* WARNING: arrays must not contain {objects} or behavior may be undefined */
+    return JSON.stringify(a1)==JSON.stringify(a2);
+}
+
 function keyPressed(e) {
 	let keyCode = e.keyCode;
-
 
 	if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
         e.preventDefault();
@@ -88,14 +106,40 @@ function keyPressed(e) {
 			newGameState = left(gridState);
 		}
 	}
+	if (arraysEqual(newGameState, gridState)) {
+		return;
+	}
 	if (newGameState != undefined) {
 		gridState = newGameState;
 		addTwoOrFour();
+
+		scoreLabel.innerHTML = "Score: " + gameScore;
+
+
+		if (isBoardFull()) {
+			gameRunning = !checkIfGameOver();
+		}
 	}
-
-
 }
 
+function enterGameOverState() {
+	ctx.fillStyle = "rgba(100, 100, 100, 0.6)";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	ctx.fillStyle = "rgb(0, 0, 0)";
+	ctx.font = "100px myFont";
+	ctx.textAlign = 'center';
+	ctx.fillText("GAME OVER", canvas.width/2, canvas.height/2);
+}
+
+function checkIfGameOver() {
+	if (arraysEqual(up(gridState), gridState) && arraysEqual(down(gridState), gridState) && 
+	    arraysEqual(right(gridState), gridState) && arraysEqual(left(gridState), gridState)) {
+			return true;
+		}
+	return false;
+}
+ 
 
 function cover_up(mat) {
     let newGrid = [];
@@ -126,6 +170,7 @@ function merge(mat, done) {
     for (let i = 0; i < 4; i++) {
         for (let j = 0; j < 4; j++) {
             if (mat[i][j] == mat[i][j+1] && mat[i][j] != 0) {
+				gameScore += mat[i][j] * 2;
                 mat[i][j] *= 2;
                 mat[i][j+1] = 0;
                 done = true;
@@ -192,25 +237,17 @@ function reverse(mat) {
 			newList[i].push(mat[i][4-j-1]);
 		}
 	}
-	console.log(newList);
     return newList;
 }
 
 
 function step(timeStep) {
-	if (start === undefined) {
-		start = timeStep
-	}	
-	let elapsed = timeStep - start;
-
-	
-	
 	draw();
 
-
-	if (elapsed < 100000) {
-		previousTimeStep = timeStep;
+	if (gameRunning) {
 		window.requestAnimationFrame(step);
+	} else {
+		enterGameOverState();
 	}
 
 }
@@ -218,5 +255,11 @@ function step(timeStep) {
 sButton.addEventListener("click", function() {
 	gameRunning = true;
 	window.requestAnimationFrame(step);
+
+	gameScore = 0;
+	gridState = [[0, 0, 0, 0],
+				 [0, 0, 2, 0],
+				 [0, 2, 0, 0],
+				 [0, 0, 0, 0]];
 
 })
